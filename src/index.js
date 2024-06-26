7fa3d1cf73bd9ea77d727f296accf7af2285f3c0
mrBidone <ab.bz@ukr.net>
2024-06-26 22:46:21 +0200

    Add more completed tasks

--- src/index.js
+++ src/index.js
@@ -337,4 +337,306 @@
 
 // 1. Напиши цикл, який виведе кожну літеру рядочку "Hello world" поки не зустріне пробіл.
 
+// function taskM22T5(str) {
+//   let shortString = '';
+//   const spaceIndex = str.indexOf(' ');
+
+//   if (spaceIndex !== -1) {
+//     for (let i = 0; i <= spaceIndex - 1; i++) {
+//       shortString += str[i];
+//     }
+//   } else {
+//     return str;
+//   }
+//   return shortString;
+// }
+
+// console.log(taskM22T5('Hello world'));
+// console.log(taskM22T5('stringWithoutSpaces'));
+// console.log(taskM22T5('My function is completed'));
+
 // 1. Напиши цикл, який виведе на консоль кожну другу літеру слова "JavaScript".
+
+// function taskM22T6(str) {
+//   let newString = '';
+
+//   for (let i = 1; i < str.length; i += 2) {
+//     newString += str[i];
+//   }
+//   return newString;
+// }
+
+// console.log(taskM22T6('JavaScript'));
+// console.log(taskM22T6('Kherson'));
+
+// 1. Напиши функцію, яка приймає рядок та повертає рядок де кожен другий символ у верхньому регістрі
+
+// function taskM22T7(str) {
+//   let newString = '';
+//   for (let i = 0; i < str.length; i++) {
+//     if (i % 2 != 0) {
+//       newString += str[i].toUpperCase();
+//     } else {
+//       newString += str[i];
+//     }
+//   }
+//   return newString;
+// }
+
+// console.log(taskM22T7('javascript'));
+
+// 1. Напиши функцію getString(str1, str2, count). Функція повинна додати str2 до рядочка str1 стільки разів скільки вказали у параметрі count.
+
+// function getString(str1, str2, count) {
+//   let newString = str1 + str2.repeat(count);
+//   return newString;
+// }
+
+// console.log(getString('STR1', '=!!=', 5));
+
+//===============================MODULE-3-1============================================
+
+// 1. Напиши функцію, яка приймає масив та виводить кожен елемент масиву у консоль.
+// function taskM31T1(array) {
+//   for (let element of array) {
+//     console.log(element);
+//   }
+// }
+
+// taskM31T1(['arr1', 'arr2', 'arr3', 'arr4', 'arr5']);
+
+// 1. Напиши функцію, яка приймає масив та виводить кожен парний елемент масиву у консоль.
+
+// function taskM31T2(array) {
+//   for (let element of array) {
+//     if (array.indexOf(element) % 2 !== 0) {
+//       console.log(element);
+//     }
+//   }
+// }
+
+// taskM31T2(['arr1', 'arr2', 'arr3', 'arr4', 'arr5']);
+
+// 1. Напиши функцію, яка приймає масив чисел та виводить лише парні числа у консоль.
+
+// function taskM31T3(array) {
+//   for (let element of array) {
+//     if (element % 2 === 0) {
+//       console.log(element);
+//     }
+//   }
+// }
+
+// taskM31T3([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// 1. Напиши функцію, яка приймає два параметри (arr, max). Де arr - це масив чисел. Потрібно вивести лише ті елементи масиву які більші за max.
+
+// function taskM31T4(numbers, max) {
+//   for (const number of numbers) {
+//     if (number > max) {
+//       console.log(number);
+//     }
+//   }
+// }
+
+// taskM31T4([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+
+// 1. Напиши функцію яка приймає масив та повертає суму всіх чисел у масиві.
+
+// function taskM31T5(numbers) {
+//   let total = 0;
+//   for (const number of numbers) {
+//     total += number;
+//   }
+//   console.log(total);
+// }
+
+// taskM31T5([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// 1. Напиши функцію яка приймає масив та елемент для пошуку. Фукнція повинна порахувати скільки разів шуканий елемент зустрічається у масиві.
+
+// function taskM31T6(numbers, searchElem) {
+//   let sumElems = 0;
+
+//   for (const number of numbers) {
+//     if (number === searchElem) {
+//       sumElems += 1;
+//     }
+//   }
+//   console.log(sumElems);
+// }
+
+// taskM31T6([1, 2, 3, 4, 6, 7, 8, 9, 1, 2, 3, 6, 7, 8, 9, 1, 2, 3, 6, 7, 8], 9);
+
+// 1. Напиши функцію яка приймає масив чисел. Функція повинна видалити усі негативні(відємні числа) з масиву та повернути новий масив.
+
+// function taskM31T7(numbers) {
+//   const newArray = [];
+//   for (const number of numbers) {
+//     if (number > 0) {
+//       newArray.push(number);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(taskM31T7([1, 2, -2, -3, 3, 4, -5, 5, 6, 7, 8, -8, 9, -9]));
+// 1. Напиши функцію яка приймає масив рядочків. Потрібно змінити кожен елемент масиву наступним чином - якщо довжина рядочку менша за 6 до привести рядок до нижнього регістру, в іншому випадку привести до верхнього регістру.
+
+// function taskM31T8(array) {
+//   const newArray = [];
+//   for (const element of array) {
+//     if (element.length <= 6) {
+//       newArray.push(element.toLowerCase());
+//     } else {
+//       newArray.push(element.toUpperCase());
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(
+//   taskM31T8(['ukraine', 'usa', 'favorite', 'ajax', 'congratulations']),
+// );
+
+// 1. Напиши функцію, яка приймає масив чисел і повертає новий масив, що містить лише непарні числа.
+
+// function taskM31T9(arr) {
+//   const newArray = [];
+//   for (const element of arr) {
+//     if (element % 2 != 0) {
+//       newArray.push(element);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(taskM31T9([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
+
+// 1. Напиши функцію, яка приймає масив чисел і повертає новий масив, де кожне число помножене на індекс, за яким воно знаходиться в масиві.
+
+// function taskM31T10(arr) {
+//   const newArray = [];
+//   for (const number of arr) {
+//     newArray.push(number * arr.indexOf(number));
+//   }
+//   return newArray;
+// }
+
+// console.log(
+//   taskM31T10([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
+// );
+
+// 1. Напиши функцію, яка приймає масив чисел і повертає новий масив, що містить лише числа, які діляться на 3.
+
+// function taskM31T11(arr) {
+//   const newArray = [];
+//   for (const number of arr) {
+//     if (number % 3 === 0) {
+//       newArray.push(number);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(
+//   taskM31T11([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]),
+// );
+
+// ## Середні задачі
+
+// 1. Напиши функцію яка приймає два масиви (arr1, arr2), та повертає новий масив де будуть лише ті елементи які зустрічаються і в arr1 і arr2.
+
+// function taskM31T12(arr1, arr2) {
+//   const newArray = [];
+//   for (const element of arr1) {
+//     if (arr2.includes(element)) {
+//       newArray.push(element);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(
+//   taskM31T12(
+//     ['BMW', 'Mercedes-Benz', 'Audi', 'VW'],
+//     ['Toyota', 'Mercedes-Benz', 'Tesla', 'Audi', 'Mitsubishi', 'VW'],
+//   ),
+// );
+
+// 1. Напиши функцію яка приймає два масиви (arr1, arr2), та повертає новий масив де будуть лише елементи масиву arr1 яких не має у масиві arr2.
+
+// function taskM31T13(arr1, arr2) {
+//   const newArray = [];
+//   for (const element of arr1) {
+//     if (!arr2.includes(element)) {
+//       newArray.push(element);
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(
+//   taskM31T13(
+//     ['BMW', 'Mercedes-Benz', 'Audi', 'VW'],
+//     ['Toyota', 'Mercedes-Benz', 'Tesla', 'Audi', 'Mitsubishi', 'VW'],
+//   ),
+// );
+
+// 1. Напиши функцію яка приймає масив та знаходить мінімальний елемент.
+
+// function taskM31T14(arr) {
+//   let minValue = arr[0];
+//   for (const number of arr) {
+//     if (minValue > number) {
+//       minValue = number;
+//     }
+//   }
+//   return minValue;
+// }
+
+// console.log(taskM31T14([12, 15, 18, 21, 13, 8, 34, 6, 9, 11, 4, 20]));
+
+// 1. Напиши функцію яка приймає масив та знаходить максимальний елемент.
+
+// function taskM31T14(arr) {
+//   let maxValue = arr[0];
+//   for (const number of arr) {
+//     if (maxValue < number) {
+//       maxValue = number;
+//     }
+//   }
+//   return maxValue;
+// }
+
+// console.log(
+//   taskM31T14([12, 15, 18, 21, 13, 8, 34, 6, 9, 11, 4, 20, 99, 150, 23]),
+// );
+
+// 1. Напиши функцію яка приймає масив та повертає масив лише з тих елементів які більші за середнє значення всіх елементів масиву.
+
+function taskM31T15(arr) {
+  let totalSum = 0;
+  const newArray = [];
+
+  for (const number of arr) {
+    totalSum += number;
+  }
+}
+
+const arrLength = arr.length;
+const averageValue = totalSum / arrLength;
+
+if (number > averageValue) {
+  newArray.push(number);
+
+  return newArray;
+}
+
+console.log(
+  taskM31T15([12, 15, 18, 21, 13, 8, 34, 6, 9, 11, 4, 20, 99, 150, 23, 22]),
+);
+
+// 1. Напиши функцію яка приймає масив та повертає масив лише з тих елементів які більші за власний індекс.
+// 1. Напишіть функці яка приймає масив чисел. Функція повинна занйти мінімальний елемент та видалити його з масиву.
+// 1. Напиши функцію яка приймає два параметри start та end. Функія повина повернути масив де будуть числа з діапазону але лише ті у яких не має цифри 5. К прикладу числа (51, 25, 15,256) не підходять бо містять символ "5" у собі.
